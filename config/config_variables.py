5776be13ef46a78db1b02c80901b469adcfbc629
MichelleGel <michellegelves@gmail.com>
2026-01-13 12:23:15 +0100

    Feat:DB connection

---
+++ config/config_variables.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+# Cargar variables del .env
+load_dotenv()
+
+class Settings:
+    DB_USER: str = os.getenv("DB_USERNAME", "default_user")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_HOST: str = os.getenv("DB_HOST", "localhost")
+    DB_NAME: str = os.getenv("DB_DEV_NAME", "volunteer_crud")
+    DB_TEST_NAME: str = os.getenv("DB_TEST_NAME", "volunteer_crud_test")
+
+
+settings = Settings()
