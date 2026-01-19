fd1f2c99c3cae1926a4fcbf4de9e5f32031bab7a
ingridD2707 <im938407@gmail.com>
2026-01-19 12:36:10 +0100

    feat:soft removal added to the model #19

--- models/category_model.py
+++ models/category_model.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Integer, String, Text
+from sqlalchemy import Integer, String, Text, DateTime
+from datetime import datetime
 from database.database import Base
 
 class Category(Base):
@@ -7,5 +8,6 @@ class Category(Base):
     id:Mapped[int]= mapped_column(Integer, primary_key=True, autoincrement=True)
     name:Mapped[str]= mapped_column(String(100), nullable=False)
     description: Mapped[str | None] = mapped_column(Text)   
+    deleted_at: Mapped[datetime | None] = mapped_column( DateTime, nullable= True, default= None)
     
     projects = relationship("Project", back_populates="category")
