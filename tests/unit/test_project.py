0cfef5c2ea9d0cd5ee4623ac0ed02ab8efb81b2a
ingridD2707 <im938407@gmail.com>
2026-01-22 12:28:54 +0100

    feat: conflicto is status and priority resolved, #3

--- tests/unit/test_project.py
+++ tests/unit/test_project.py
@@ -7,6 +7,7 @@ from schemas.project_schema import ProjectCreate
 from schemas.category_schemas import CategoryCreate
 from models.project_model import Project
 from controllers.category_controller import create_category
+from domain.projects_enums import Project_status, Project_priority
 
 
 @pytest.mark.asyncio
@@ -23,8 +24,8 @@ async def test_create_project_success(db_session):
         name="Proyecto A",
         description="Proyecto de prueba",
         deadline=datetime.utcnow() + timedelta(days=10),
-        status="assigned",
-        priority="high",
+        status=Project_status.assigned,     
+        priority=Project_priority.high,     
         category_id=category.id
     )
 
@@ -34,21 +35,25 @@ async def test_create_project_success(db_session):
     assert saved is not None
     assert saved.name == "Proyecto A"
     assert saved.category_id == category.id
+    assert saved.status == Project_status.assigned
 
 
 @pytest.mark.asyncio
 async def test_create_project_duplicate_name(db_session):
     category = create_category(
         db_session,
-        CategoryCreate(name="Cat Dup", description="desc")
+        CategoryCreate(
+            name="Cat Dup",
+            description="desc"
+        )
     )
 
     data = ProjectCreate(
         name="Proyecto Duplicado",
         description="desc",
         deadline=datetime.utcnow(),
-        status="assigned",
-        priority="medium",
+        status=Project_status.assigned,      
+        priority=Project_priority.medium,   
         category_id=category.id
     )
 
@@ -57,4 +62,4 @@ async def test_create_project_duplicate_name(db_session):
     with pytest.raises(HTTPException) as exc:
         await ProjectController.create_item(db_session, data)
 
-    assert exc.value.status_code == 400
+    assert exc.value.status_code == 409
