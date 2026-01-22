0b476ea148310d1a4154bad0f7a08ffc25a680c5
ingridD2707 <im938407@gmail.com>
2026-01-22 12:25:55 +0100

    feat: imports arranged #3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,14 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database.database import Base 
-from models import  User, Volunteer, Skill, volunteer_skills, Project, Category
-from config.config_variables import settings 
 from sqlalchemy.exc import OperationalError
 
-# Construir URL de conexión usando Settings
+from database.database import Base
+import models  
+
+from config.config_variables import settings
+
+
 DATABASE_URL = (
     f"{settings.DB_DIALECT}://{settings.DB_USERNAME}:"
     f"{settings.DB_PASSWORD}@{settings.DB_HOST}:"
@@ -14,18 +16,18 @@ DATABASE_URL = (
     f"{settings.DB_TEST_NAME}"
 )
 
-# Crear motor y sesión
 engine = create_engine(DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
 @pytest.fixture(scope="function")
 def db_session():
-    # Crear tablas antes de cada test
     try:
         Base.metadata.create_all(bind=engine)
     except OperationalError as e:
         raise RuntimeError(
-            f"No se pudo conectar a la base de datos de test. "
-            f"Revisa que exista '{settings.DB_TEST_NAME}' y que MySQL esté corriendo.\nError: {e}"
+            f"No se pudo conectar a la base de datos de test "
+            f"'{settings.DB_TEST_NAME}'.\nError: {e}"
         )
 
     db = TestingSessionLocal()
